dbdf396d2f1833d4ba9d5e12e4284709464e97c7
PC26 <PC26@Cpu26>
2026-01-31 09:45:04 -0600

    codigoPrincipal

---
+++ src/pruebaequipo2/Pruebaequipo2.java
@@ -0,0 +1,24 @@
+
+package pruebaequipo2;
+
+import java.util.Scanner;
+
+public class Pruebaequipo2 {
+
+    public static void main(String[] args) {
+        Scanner nn = new Scanner (System.in);
+        
+        System.out.println("Ingresa el primer numero: ");
+        int a = nn.nextInt();
+        
+        System.out.println("Ingresa el segundo numero: ");
+        int b = nn.nextInt();
+//        
+//        Multiplicacion multi = new Multiplicacion();
+//        Division divi = new Division ();
+//        
+//        System.out.println("El resultado de la multiplicacion es: "+Multiplicacion.operacion(a,b));
+//        System.out.println("El resultado de la multiplicacion es: "+Division.operacion(a,b));
+    }
+    
+}
